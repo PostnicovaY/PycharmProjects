0695694ec0e8f4b1d206923f9688540f38c5b4bc
Postnikova_Yana <oceana91@rambler.ru>
2021-06-21 19:58:41 +0300

    удалила лишнее

--- Postnikova_Yana_dz_4.4.py
+++
@@ -1,4 +0,0 @@
-from utils import currency_rates
-currency_rates("usd")
-currency_rates("xnj") #Как было указано в задании: возвращаем None, поэтому эту строчку просто не покажет
-currency_rates("BGN")
